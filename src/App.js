baed8bb3eda4b6c150c5c67b171fcce439d3aefd
mozahid34 <aymanmozahid@gmail.com>
2022-04-08 00:57:28 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ButtonGroup, Card, Col, Form, Row } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import News from './components/News/News';
+
 
 function App() {
+   const [news, setNews] = useState([])
+   useEffect(() => {
+     fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=027161a93a4749838213e9e46d2c18c3')
+      .then(res => res.json())
+      .then(data => setNews(data.articles))
+   },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row xs={1} md={2} className="g-4">
+        {
+          news.map(nw => <News news = {nw} ></News> )
+    
+        }
+  
+    
+  
+</Row>
     </div>
   );
 }
